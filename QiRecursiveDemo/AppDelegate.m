d29156e740405f814825b0a0a99dfd6d0968717e
wangyongwang <wangyongwang@360.cn>
2018-08-22 12:19:27 +0800

    递归尾递归简单介绍

--- QiRecursiveDemo/AppDelegate.m
+++ QiRecursiveDemo/AppDelegate.m
@@ -2,11 +2,12 @@
 //  AppDelegate.m
 //  QiRecursiveDemo
 //
-//  Created by wangyongwang on 2018/8/22.
-//  Copyright © 2018年 qishare. All rights reserved.
+//  Created by QiShare on 2018/8/22.
+//  Copyright © 2018年 QiShare. All rights reserved.
 //
 
 #import "AppDelegate.h"
+#import "QiRecursiveViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,35 +17,13 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    _window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    _window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[QiRecursiveViewController new]];
+    _window.backgroundColor = [UIColor whiteColor];
+    [_window makeKeyAndVisible];
+    
     return YES;
-}
-
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-}
-
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-}
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 
--- QiRecursiveDemo/QiRecursiveViewController.h
+++ QiRecursiveDemo/QiRecursiveViewController.h
@@ -1,5 +1,5 @@
 //
-//  AppDelegate.h
+//  QiRecursiveViewController.h
 //  QiRecursiveDemo
 //
 //  Created by QiShare on 2018/8/22.
@@ -8,10 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
-
+@interface QiRecursiveViewController : UIViewController
 
 @end
-
